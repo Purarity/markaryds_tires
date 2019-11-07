21d165fbf8aca075b333ecc3d021457ee6fc2197
Purarity <satthu_rong55@yahoo.com>
2019-11-07 22:22:14 +0100

    Added price filtering

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+
+const ListGroup = ({ groups, onGroupSelect, selectedGroup }) => {
+  return (
+    <React.Fragment>
+      {groups.map(price => {
+        let classes = "list-group-item";
+        classes += selectedGroup === price.range ? " active" : "";
+        return (
+          <li
+            key={price.range}
+            className={classes}
+            onClick={() => onGroupSelect(price)}
+          >
+            {price.range}
+          </li>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+ListGroup.propType = {
+  groups: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onItemSelect: PropTypes.func.isRequired
+};
+
+export default ListGroup;
--- src/components/displayAllProducts.jsx
+++ src/components/displayAllProducts.jsx
@@ -3,12 +3,26 @@ import { Link } from "react-router-dom";
 import { getTiresList, deleteProduct } from "../api/fakeApi";
 import Table from "./common/table";
 import SearchBox from "./common/searchBox";
+import ListGroup from "./common/listGroup";
 
 class DisplayAllProducts extends Component {
-  state = { data: [], searchQuery: "" };
+  state = {
+    data: [],
+    searchQuery: "",
+    priceGroups: [],
+    selectedGroup: "All Prices"
+  };
 
   componentDidMount() {
-    this.setState({ data: getTiresList() });
+    const priceGroups = [
+      { range: "All Prices" },
+      { range: "500-600" },
+      { range: "601-700" },
+      { range: "701-800" },
+      { range: "801-900" },
+      { range: "901-1000" }
+    ];
+    this.setState({ data: getTiresList(), priceGroups });
   }
 
   handleDelete = productId => {
@@ -20,30 +34,86 @@ class DisplayAllProducts extends Component {
     this.setState({ searchQuery });
   };
 
+  handleGroupSelect = group => {
+    group.range === "All Prices"
+      ? this.setState({ selectedGroup: "All Prices" })
+      : this.setState({ selectedGroup: group.range });
+
+    this.setState({ currentPage: 1, searchQuery: "" });
+  };
+
   getVisibleProducts = () => {
-    const { data, searchQuery } = this.state;
-    let searchedProducts;
+    const { data, searchQuery, selectedGroup } = this.state;
+    let displayedProducts;
     if (searchQuery) {
-      searchedProducts = data.filter(product =>
+      displayedProducts = data.filter(product =>
         product.name.toLowerCase().includes(searchQuery.toLowerCase())
       );
     } else {
-      return data;
+      switch (selectedGroup) {
+        //#region filter prices
+        case "500-600":
+          displayedProducts = data.filter(
+            item => item.price >= 500 && item.price < 600
+          );
+          break;
+        case "601-700":
+          displayedProducts = data.filter(
+            item => item.price >= 601 && item.price < 700
+          );
+          break;
+        case "701-800":
+          displayedProducts = data.filter(
+            item => item.price >= 701 && item.price < 800
+          );
+          break;
+        case "801-900":
+          displayedProducts = data.filter(
+            item => item.price >= 801 && item.price < 900
+          );
+          break;
+        case "901-1000":
+          displayedProducts = data.filter(
+            item => item.price >= 901 && item.price <= 1000
+          );
+          break;
+
+        default:
+          displayedProducts = data;
+          break;
+        //#endregion
+      }
+      return displayedProducts;
     }
-    return searchedProducts;
+    return displayedProducts;
   };
 
   render() {
-    const { searchQuery } = this.state;
+    const { searchQuery, priceGroups, selectedGroup } = this.state;
     const products = this.getVisibleProducts();
     return (
       <React.Fragment>
-        <div className="p-3" align="left">
-          <Link to="/product/new" className="btn btn-primary">
-            New Product
-          </Link>
+        <div className="p-3">
+          <div className="row">
+            <Link to="/product/new" className="btn btn-primary">
+              New Product
+            </Link>
+            <div className="col">
+              <SearchBox value={searchQuery} onChange={this.handleSearch} />
+            </div>
+          </div>
+          <div>
+            <div className="pt-2" align="left">
+              <div className="row">
+                <ListGroup
+                  groups={priceGroups}
+                  selectedGroup={selectedGroup}
+                  onGroupSelect={this.handleGroupSelect}
+                ></ListGroup>
+              </div>
+            </div>
+          </div>
         </div>
-        <SearchBox value={searchQuery} onChange={this.handleSearch} />
         <Table data={products} onDelete={this.handleDelete}></Table>
       </React.Fragment>
     );
